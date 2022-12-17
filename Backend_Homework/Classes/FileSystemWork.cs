72b6de75e51723c6bece9791239edabddf7a4ba7
Mykola Matios <65984494+Mikolka09@users.noreply.github.com>
2022-12-17 22:03:35 +0100

    17.12.2022

---
+++ Backend_Homework/Classes/FileSystemWork.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backend_Homework.Classes
+{
+    internal class FileSystemWork
+    {
+        private static string? SourceFileName { get; set; }
+        private static string? TargetFileName { get; set; }
+
+        public static string? TextDocument { get; private set; }
+
+
+        public static void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\t\tFILE SYSTEM MENU\n");
+                Console.Write("\tThe required file is in the folder - \"Source Files\" (Yes - 1, NO - 2): ");
+                int var = int.Parse(Console.ReadLine());
+                switch (var)
+                {
+                    case 1:
+                        FilePathCreate();
+                        break;
+                    case 2:
+                        FileFullPathCreate();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        //получаем от пользователя полный путь к файлу
+        private static void FileFullPathCreate()
+        {
+            //We accept the path from the user, followed by input validation
+            Console.Write("Enter the full path to the file: ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
+                {
+
+                    if (!string.IsNullOrEmpty(Path.GetExtension(input)))
+                    {
+                        SourceFileName = input;
+                        FileOpenRead();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n[ERROR]: Missing file extension");
+                        Console.Write("Retry Enter the full path to the file: ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n[ERROR]: Invalid input");
+                    Console.Write("Retry Enter the full path to the file: ");
+                }
+            }
+        }
+
+        //we get the full file name from the user and create a path to them
+        private static void FilePathCreate()
+        {
+            //We accept the path from the user, followed by input validation
+            Console.Write("Enter the full name to the file: ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
+                {
+                    if (!string.IsNullOrEmpty(Path.GetExtension(input)))
+                    {
+                        SourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\" + input);
+                        FileOpenRead();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n[ERROR]: Missing file extension");
+                        Console.Write("Retry Enter the full name to the file: ");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("\n[ERROR]: Invalid input");
+                    Console.Write("Retry Enter the full name to the file: ");
+                }
+            }
+        }
+
+        //opening and reading a file
+        private static void FileOpenRead()
+        {
+            if (File.Exists(SourceFileName))
+            {
+                using (FileStream fileStream = File.Open(SourceFileName, FileMode.Open))
+                {
+                    using (StreamReader stream = new StreamReader(fileStream))
+                    {
+                        string input = stream.ReadToEnd();
+                        if (!string.IsNullOrEmpty(input))
+                        {
+                            TextDocument = input;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n[ERROR]: Read error or file is empty");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n[ERROR]: The path to the file is incorrect or the file does not exist");
+            }
+        }
+    }
+}
--- Backend_Homework/Program.cs
+++ Backend_Homework/Program.cs
@@ -1,43 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Claims;
 using System.Xml.Linq;
+using Backend_Homework.Classes;
 using Newtonsoft.Json;
-namespace Continero.Homework
+namespace Backend_Homework
 {
-    public class Document
+    #region old buggy code
+    //it is better to create a class in a separate file but in the same space, and call the file the name of the class
+    /*public class Document
     {
+        //perhaps it should be added that the property can be null - "public string? Title { get; set; }"
         public string Title { get; set; }
+
+        //perhaps it should be added that the property can be null - "public string? Text { get; set; }"
         public string Text { get; set; }
-    }
+    }*/
+    #endregion
     class Program
     {
         static void Main(string[] args)
         {
-            var sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
 
+          
 
+            #region old buggy code
+            //the folder and file were not created at the given path, it will throw an exception "FileNotFoundException"
+            /*var sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
+
+            //the folder has not been created at the given path, it will give an error when trying to write a file to this folder
             var targetFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Target Files\\Document1.json");
-        try
+            try
             {
+                //at the beginning, you need to check for the existence of a file along this path so that an exception
+                //does not pop up in this way: if(File.Exists(sourceFileName)){}, and if such a file exists, then open it
+
                 FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
                 var reader = new StreamReader(sourceStream);
+
+                //the "input" string must be declared before try, as it will not be available to subsequent code (it is not in scope)
                 string input = reader.ReadToEnd();
+
+                //you need to close the FileStream.Close() and StreamReader.Close() streams here in "try", since in the future
+                //the next opening of the stream or accessing the file will lead to an error, or instead of "try{}catch{}"
+                //use the "using(){}", which will automatically release all thread-related resources
+
             }
             catch (Exception ex)
             {
+                //very bad option, it loses the original stack trace of the exception, as well as its type, this will destroy
+                //all information about the exception
                 throw new Exception(ex.Message);
             }
+
+            //the string "input" is not visible because it is not visible
             var xdoc = XDocument.Parse(input);
-            var doc = new Document
+
+            //first you need to check "xdoc.Root" for "is not null"
+            var doc = new Document()
             {
+                //when dereferencing a variable whose value is null, the runtime will throw an exception.
+                //To do this, you need before the operator "." put the negation operator "!" NULL
                 Title = xdoc.Root.Element("title").Value,
                 Text = xdoc.Root.Element("text").Value
             };
+          
             var serializedDoc = JsonConvert.SerializeObject(doc);
+
+            //you need to work with streams in "try{}catch{}" and close the streams, or immediately use the "using(){}" construct,
+            //which will automatically release all resources associated with the streams. Also, if the streams are not closed,
+            //the information will not be written to the file.
+            //And it's better to use the method - "File.WriteAllText(targetFileName, serializedDoc)"
+
             var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
             var sw = new StreamWriter(targetStream);
-            sw.Write(serializedDoc);
+            sw.Write(serializedDoc);*/
+            #endregion
         }
     }
 }
